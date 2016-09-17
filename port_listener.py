670c11d4f26b9914e4bf6b9f751c6a751ffbe0eb
CallumGill <callum.gill001@gmail.com>
2016-09-17 21:35:16 +1200

    thread port testing 2

--- port_listener.py
+++ port_listener.py
@@ -1,4 +1,4 @@
-import serial, socket, threading, Queue
+import serial, socket, threading, Queue, time
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 8082              # Arbitrary non-privileged port
@@ -12,6 +12,8 @@ serial_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def listener():
 	while(True):
+		if not r_command_queue.empty():
+			print r_command_queue.get()
 		if not command_queue.empty():
 			cmd = command_queue.get()
 			conn.sendAll(cmd)
@@ -19,12 +21,23 @@ def listener():
 		if recieved:
 			r_command_queue.put(recieved)
 
+def command_creator():
+	count = 1
+	while(True):
+		time.sleep(2)
+		command_queue.put('Command : ' + count)
+		count = count + 1
+
+
 def startListener():
 	if not connected:
 		connect()
 	t = threading.Thread(target=listener())
+	c = threading.Thread(target=command_creator())
+	c.daemon = True
 	t.daemon = True
 	t.start()
+	c.start()
 
 def connect():
 	while not conn:
@@ -32,3 +45,5 @@ def connect():
 		serial_socket.listen(1)
 		conn, adr = serial_socket.accept()
 	connected = True
+
+startListener()
