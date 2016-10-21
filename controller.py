4e8121907f4a1dfe06ea02b54bc393cf332b7922
chauhamite <chauhamite@ecs.vuw.ac.nz>
2016-10-21 22:55:17 +1300

    Code Quality: Python Fixed up the code styling on the python files and ran pylint.

--- controller.py
+++ controller.py
@@ -1,48 +1,58 @@
+"""
+	:author: Miten Chauhan
+
+    This file holds the functions required to send a command over the
+    network to control a node. It also holds the function to identify which
+    COM port the coordinator is plugged in to.
+"""
+
+import time
 import serial
 import serial.tools.list_ports
-import time
 
-"""
-Send a command over the network.
-"""
-def start(com, speed, direction):
+def send_command(com, speed, direction):
+    """Send a command over the network.
+
+    Sends a command over the network containing the nodes name to identify
+    which node is the intended target, the direction that node should be
+    travelling in, and the speed the node should be travelling at.
+
+    Args:
+        com: The COM port the coordinator is plugged in to. This is so the
+            program knows which port to send the command through.
+        speed: the speed the node should be travelling at.
+        direction: the direction the node should be travelling in.
+    """
     print '-------- STARTING --------'
     port = serial.Serial(com, baudrate=9600, timeout=0)
     node = 'BELLE'
-    port.write('~' + node + '*' + direction + '*' + speed + '*' + '#')
-    data = ''
+    command = '~{}*{}*{}*#'.format(node, direction, speed)
+    port.write(command)
+    node_response = ''
     # time.sleep is set to 1 second to give the network enough time
     # to send the full message before reading it.
     time.sleep(1)
-    data = port.readline()
-    print(data)
+    node_response = port.readline()
+    print node_response
     print '----------- FINISH -----------'
 
-"""
-Stops a node from moving.
-"""
-def stop(com, speed, direction):
-    print '-------- STOPPING --------'
-    port = serial.Serial(com, baudrate=9600, timeout=0)
-    node = 'BELLE'
-    port.write('~' + node + '*' + direction + '*' + speed + '*' + '#')
-    data = ''
-    # time.sleep is set to 1 second to give the network enough time
-    # to send the full message before reading it.
-    time.sleep(1)
-    data = port.readline()
-    print(data)
-    print '----------- FINISH -----------'
-
-
-"""
-Identify the COM port the coordinator node is connected to.
-"""
 def find_coordinater():
+    """Find the COM port for the coordinator.
+
+    Identify the COM Port the coordinator is connected to by scanning through
+    all the devices connected to com ports.
+
+    Returns:
+        The COM Port the coordinator is plugged in to, or 'unidentified' if
+        the coordinator's COM port couldn't be found. A string is returned
+        for example:
+
+            'COM5'
+    """
     com_port = 'unidentified'
     ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        if "USB Serial Port" in p[1]:
-            com_port = p[0]
-        print('===== COM Port: ' + com_port)
+    for port in ports:
+        if "USB Serial Port" in port[1]:
+            com_port = port[0]
+    print '====== COM PORT : {} ======'.format(com_port)
     return com_port
--- db_create.py
+++ db_create.py
@@ -1,12 +1,14 @@
 """
+    :author: Miten Chauhan
+
     This file holds the script to make a database using the model in db.py
 
     Before running this file note the following:
         - If there is already an 'app.db' file any new values added to this file
         will add on to the existing database.
 
-        - To create a new fresh database, move the 'app.db' file and save it 
-        somewhere else. DO NOT delete it. Keep it as a backup so you don't 
+        - To create a new fresh database, move the 'app.db' file and save it
+        somewhere else. DO NOT delete it. Keep it as a backup so you don't
         lose your old database. After, running this file will make a new
         'app.db' file in this directory and will contain all the information
         currently in this file.
@@ -15,26 +17,26 @@
 # Creates the database using the model in db.py
 # Only use the following code to create a new database.
 from db import db
+from db import Device
+
 db.create_all()
 
-
-from db import Device, Edge
 # Adding Nodes to the database.
-elsa = Device('Elsa', 'Node')
-db.session.add(elsa)
+ELSA = Device('Elsa', 'Node')
+db.session.add(ELSA)
 
-aurora = Device('Aurora', 'Node')
-db.session.add(aurora)
+AURORA = Device('Aurora', 'Node')
+db.session.add(AURORA)
 
-belle = Device('Belle', 'Node')
-db.session.add(belle)
+BELLE = Device('Belle', 'Node')
+db.session.add(BELLE)
 
 # Adding some test sensors to the database
-alpha = Device('Alpha', 'Sensor')
-db.session.add(alpha)
+ALPHA = Device('Alpha', 'Sensor')
+db.session.add(ALPHA)
 
-beta = Device('Beta', 'Sensor')
-db.session.add(beta)
+BETA = Device('Beta', 'Sensor')
+db.session.add(BETA)
 
 # Commit new information to the database
 db.session.commit()
